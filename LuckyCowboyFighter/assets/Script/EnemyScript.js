c94247d234c7ede96cace7e87f92c06a77083b18
Bruno Müller <muellerbr@cip.ifi.lmu.de>
2016-09-21 15:11:24 +0200

    Gloly to China! Danke Merkl.

--- LuckyCowboyFighter/assets/Script/EnemyScript.js
+++ LuckyCowboyFighter/assets/Script/EnemyScript.js
@@ -2,7 +2,9 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        attackRadius: 0
+        attackRadius: 0,
+        xSpeed: 0,
+        ySpeed: 0,
     },
 
     // use this for initialization
@@ -22,11 +24,31 @@ cc.Class({
         this.node.destroy();
     },
 
+    moveEnemy: function(dt){
+        var playerX = this.game.player.getPositionX();
+        var playerY = this.game.player.getPositionY();
+        var xPos = this.node.x;
+        var yPos = this.node.y;
+
+        if(yPos <= playerY){
+          this.node.y = yPos + this.ySpeed * dt;
+        }else{
+          this.node.y = yPos - this.ySpeed * dt;
+        }
+
+        if(xPos <= playerX){
+          this.node.x = xPos + this.xSpeed * dt;
+        }else{
+          this.node.x = xPos - this.xSpeed * dt;
+        }
+    },
+
     // called every frame
     update: function (dt) {
         if (this.getPlayerDistance() < this.attackRadius) {
             this.attack();
             return;
         }
+        this.moveEnemy(dt);
     },
 });
