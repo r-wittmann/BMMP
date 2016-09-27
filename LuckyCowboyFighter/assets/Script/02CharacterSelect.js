c6367127ac6aa0c21dc71e29a5f2dfc563a61e0e
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-09-27 16:41:30 +0200

    add stats to characterSelect

--- LuckyCowboyFighter/assets/Script/02CharacterSelect.js
+++ LuckyCowboyFighter/assets/Script/02CharacterSelect.js
@@ -38,7 +38,6 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        this.selectedCharacter = null;
     },
 
     // called every frame, uncomment this function to activate update callback
@@ -47,22 +46,52 @@ cc.Class({
     // },
 
     character1Select: function () {
-        this.selectedCharacter = 1;
         this.character1animation.play('selectAnim_Cowboy');
-        this.node.getChildByName('Character2').getChildByName('Profile').scale = 1;
-        this.node.getChildByName('Character3').getChildByName('Profile').scale = 1;
+        this.character1.getChildByName('Stats').opacity = 255;
+        this.character2.getChildByName('Stats').opacity = 0;
+        this.character3.getChildByName('Stats').opacity = 0;
+        cc.sys.localStorage = {
+            selectedCharacter: 1,
+            characterHealth: 100,
+            currentHealth: 100,
+            characterStrength: 20,
+            characterExperience: 0,
+            characterLevel: 1,
+            characterAttackRadius: 50,
+            stage: 1
+        }
     },
     character2Select: function () {
-        this.selectedCharacter = 2;
         this.character2animation.play('selectAnim_Knight');
-        this.node.getChildByName('Character1').getChildByName('Profile').scale = 1;
-        this.node.getChildByName('Character3').getChildByName('Profile').scale = 1;
+        this.character1.getChildByName('Stats').opacity = 0;
+        this.character2.getChildByName('Stats').opacity = 255;
+        this.character3.getChildByName('Stats').opacity = 0;
+        cc.sys.localStorage = {
+            selectedCharacter: 2,
+            characterHealth: 200,
+            currentHealth: 200,
+            characterStrength: 30,
+            characterExperience: 0,
+            characterLevel: 1,
+            characterAttackRadius: 120,
+            stage: 1
+        }
     },
     character3Select: function () {
-        this.selectedCharacter = 3;
         this.character3animation.play('selectAnim_Ninja');
-        this.node.getChildByName('Character1').getChildByName('Profile').scale = 1;
-        this.node.getChildByName('Character2').getChildByName('Profile').scale = 1;
+        this.character1.getChildByName('Stats').opacity = 0;
+        this.character2.getChildByName('Stats').opacity = 0;
+        this.character3.getChildByName('Stats').opacity = 255;
+        cc.sys.localStorage = {
+            selectedCharacter: 3,
+            characterHealth: 50,
+            currentHealth: 50,
+            characterStrength: 15,
+            characterExperience: 0,
+            characterLevel: 1,
+            characterAttackRadius: 90,
+            stage: 1
+        }
     },
     
     //back to menu
@@ -72,45 +101,5 @@ cc.Class({
     //start game
     goToGame: function () {
         cc.director.loadScene('Stages/stage01');
-        let localStorageObject = {}
-        switch (this.selectedCharacter) {
-            case 1:
-                localStorageObject = {
-                    selectedCharacter: 1,
-                    characterHealth: 100,
-                    currentHealth: 100,
-                    characterStrength: 20,
-                    characterExperience: 0,
-                    characterLevel: 1,
-                    characterAttackRadius: 50,
-                    stage: 1
-                }
-                break;
-            case 2:
-                localStorageObject = {
-                    selectedCharacter: 2,
-                    characterHealth: 200,
-                    currentHealth: 200,
-                    characterStrength: 30,
-                    characterExperience: 0,
-                    characterLevel: 1,
-                    characterAttackRadius: 120,
-                    stage: 1
-                }
-                break;
-            case 3:
-                localStorageObject = {
-                    selectedCharacter: 3,
-                    characterHealth: 50,
-                    currentHealth: 50,
-                    characterStrength: 15,
-                    characterExperience: 0,
-                    characterLevel: 1,
-                    characterAttackRadius: 90,
-                    stage: 1
-                }
-                break; 
-        }
-        cc.sys.localStorage = localStorageObject;
     },
 });
