3bd119d0092f5652fdbb7ef5f55d7e3152e7ed3a
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-09-29 16:21:34 +0200

    character and enemies are faded in and out, too

--- LuckyCowboyFighter/assets/Script/ChangeSceneScript.js
+++ LuckyCowboyFighter/assets/Script/ChangeSceneScript.js
@@ -1,6 +1,7 @@
 
 module.exports.loadScene = function (director, nextSceneName) {
     let runningSceneCanvas = director.getScene().getChildByName('Canvas')
+    runningSceneCanvas.getChildByName('Fade').zIndex = 4000;
     runningSceneCanvas.getChildByName('Fade').runAction(
         cc.sequence(
             cc.fadeIn(1),
@@ -11,6 +12,7 @@ module.exports.loadScene = function (director, nextSceneName) {
 
 module.exports.fadeIn = function (director) {
     let newSceneCanvas = director.getScene().getChildByName('Canvas')
+    newSceneCanvas.getChildByName('Fade').zIndex = 4000;
     newSceneCanvas.getChildByName('Fade').runAction(
         cc.fadeOut(1)
     );
